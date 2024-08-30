673ce4f1ff2a0ca9dbca2d113e50b160fffff3bc
myk910 <maaaaaayu3@gmail.com>
2024-08-30 17:29:24 +0900

    Add files via upload
    kadai_014アップロード

---
+++ kadai_014/square.js
@@ -0,0 +1,12 @@
+// 与えられた引数priceに送料を加算し、その値を出力する関数を定義する
+const calculateTotal = (num) => {
+  console.log(num);
+}
+
+// 与えられた引数numを2倍にし、その値を戻り値として返す関数を定義する
+const double = (num) => {
+  return num * num;
+}
+
+// 関数の戻り値を出力する
+console.log(double(10));
